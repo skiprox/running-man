d88958b48398cb12e7460543e129b7ed35dcdd3a
Sean Scanlan <sean.scanlan@kettlenyc.com>
2015-02-03 14:30:50 -0500

    adding in cool-ascii-faces, esprima, and fs

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 'use strict';
 
+var _ = require('lodash'),
+	cool = require('cool-ascii-faces'),
+	esprima = require('esprima'),
+	fs = require('fs');
+
 var RunningMan = (function() {
+
+	var esprimaTest = function(file) {
+		_.pick(esprima.parse(file), 'body').body.forEach(function(n) {
+			console.log(JSON.stringify(n) + '\n');
+		});
+	}
 
 	return {
 		init: function() {
-			console.log('Is this working?');
+			console.log(cool());
+			var code = fs.readFileSync('index.js')
+			esprimaTest(code);
 		}
 	};
 
